60c62bf22bb29a77342d2da2b579accd8b313a1a
alexander.ovchinnikov <alexander.ovchinnikov@stark.games>
2018-05-07 13:17:57 +0300

    update request params

--- Assets/Scripts/Application/Models/YleItems.cs
+++ Assets/Scripts/Application/Models/YleItems.cs
@@ -1,4 +1,5 @@
-﻿using Networking;
+﻿using System.Collections.Generic;
+using Networking;
 using UnityEngine;
 using YLE.API;
 using YLE.MVC.Model;
@@ -10,6 +11,12 @@ namespace YLE.MVC.Model
         private readonly int limit = 10;
         private int offset;
         private string q = "";
+
+        private readonly Dictionary<string, string> _requestParams = new Dictionary<string, string>()
+        {
+            {"language", "fi"},
+            {"type", "program"},
+        };
 
         public static YleItems Init(YleAPI api, MonoBehaviour mono)
         {
@@ -26,7 +33,7 @@ namespace YLE.MVC.Model
 
         public void GetNext()
         {
-            StartCoroutine(Api.Items(q, limit, offset).Get(data =>
+            StartCoroutine(Api.Items(q, limit, offset, _requestParams).Get(data =>
             {
                 SetData(data);
                 offset += limit;
--- Assets/Scripts/Networking/YLEApi/YleAPIItems.cs
+++ Assets/Scripts/Networking/YLEApi/YleAPIItems.cs
@@ -6,17 +6,27 @@ namespace YLE.API
 {
     internal static class YleAPIItems
     {
-        public static YLERequest Items(this YleAPI api, string q = "", int limit = 10, int offset = 0)
+        public static YLERequest Items(this YleAPI api, string q = "", int limit = 10, int offset = 0,
+            Dictionary<string, string> options = null)
         {
             var url = "programs/items.json";
-            return YLERequest.Init(api, url, new Dictionary<string, string>
+            var request_params = new Dictionary<string, string>()
             {
                 {"q", q},
                 {"limit", limit.ToString()},
                 {"offset", offset.ToString()},
-                {"language", "fi"},
-                {"type", "program"}
-            });
+            };
+            
+            if (options != null)
+            {
+                foreach (KeyValuePair<string, string> option in options)
+                {
+                    request_params[option.Key] = option.Value;
+                }
+            }
+
+
+            return YLERequest.Init(api, url, request_params);
         }
     }
 }
